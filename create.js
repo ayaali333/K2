5aa7905536a7a405f5644715460cdb0ffb307199
ayaali333 <1aya.dn.ali@gmail.com>
2022-03-02 20:37:55 +0300

    first commit

---
+++ create.js
@@ -0,0 +1,8 @@
+let mybutton = document.querySelector('.button-bg');
+let form = document.querySelector('.myform');
+
+
+mybutton.addEventListener ('click',function(){
+    form.classList.add(".bg-active");
+});
+
